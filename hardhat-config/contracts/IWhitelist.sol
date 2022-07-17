5501a4597b82b84a4eaa9b93f64b2021c4aad644
Y.M. Cheung <y.m.cheung@YMs-MacBook-Pro.local>
2022-07-17 17:15:47 +0200

    fix contracts

--- hardhat-config/contracts/IWhitelist.sol
+++ hardhat-config/contracts/IWhitelist.sol
@@ -5,5 +5,5 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 interface IWhitelist {
-    function whitelistedAddresses(address) external vuew returns (bool);
+    function whitelistedAddresses(address) external view returns (bool);
 }
--- hardhat-config/contracts/RunningDevs.sol
+++ hardhat-config/contracts/RunningDevs.sol
@@ -46,52 +46,57 @@ contract RunningDevs is ERC721Enumerable, Ownable {
         whitelist = IWhitelist(whitelistContract);
     }
 
-		/**
-      * @dev startPresale starts a presale for the whitelisted addresses
-       */
+    /**
+     * @dev startPresale starts a presale for the whitelisted addresses
+     */
     function startPresale() public onlyOwner {
         presaleStarted = true;
         presaleEnded = block.timestamp + 10 minutes;
     }
 
-		/**
-       * @dev presaleMint allows a user to mint one NFT per transaction during the presale.
-       */
+    /**
+     * @dev presaleMint allows a user to mint one NFT per transaction during the presale.
+     */
 
-		function presaleMint() public payable onlyWhenNotPaused {
-			require(presaleStarted && block.timestamp < presaleEnded, "Presale is not running");
-			require(whitelist.whitelistedAddresses(msg.sender), "You are not on the whitelist");
-			require(tokenIds < maxTokenIds, "Exceeded maximum Running Devs supply");
-			require(msg.value >= _price, "Ether is not sufficient");
+    function presaleMint() public payable onlyWhenNotPaused {
+        require(
+            presaleStarted && block.timestamp < presaleEnded,
+            "Presale is not running"
+        );
+        require(
+            whitelist.whitelistedAddresses(msg.sender),
+            "You are not on the whitelist"
+        );
+        require(tokenIds < maxTokenIds, "Exceeded maximum Running Devs supply");
+        require(msg.value >= _price, "Ether is not sufficient");
 
-			tokenIds +=1;
-			_safeMint(msg.sender, tokenIds);
-		}
-
-		 /**
-      * @dev _baseURI overides the Openzeppelin's ERC721 implementation which by default
-      * returned an empty string for the baseURI
-      */
-
-		function _baseURI() internal view virtual override returns (string memory) {
-			return _baseTokenURI;
-		}
-
-		function setPaused(bool val) public onlyOwner {
-			_paused = val;
-		}
-
-		function withdraw() public onlyOwner  {
-    	address _owner = owner();
-      uint256 amount = address(this).balance;
-      (bool sent, ) =  _owner.call{value: amount}("");
-      require(sent, "Failed to send Ether");
+        tokenIds += 1;
+        _safeMint(msg.sender, tokenIds);
     }
 
-		// Function to receive Ether. msg.data must be empty
+    /**
+     * @dev _baseURI overides the Openzeppelin's ERC721 implementation which by default
+     * returned an empty string for the baseURI
+     */
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function setPaused(bool val) public onlyOwner {
+        _paused = val;
+    }
+
+    function withdraw() public onlyOwner {
+        address _owner = owner();
+        uint256 amount = address(this).balance;
+        (bool sent, ) = _owner.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+    }
+
+    // Function to receive Ether. msg.data must be empty
     receive() external payable {}
 
     // Fallback function is called when msg.data is not empty
     fallback() external payable {}
-  }
 }
